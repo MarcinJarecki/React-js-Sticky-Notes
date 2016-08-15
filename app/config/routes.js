915d364173e56e976edc87f05e28d92b84a3c176
MarcinJarecki <m.jarecki78@gmail.com>
2016-08-15 12:21:56 +0200

    Add route configuration - part 1

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
-import Main from '../index.js';
+import Main from '../components/main.js';
 import Config from '../components/config';
 import About from '../components/about';
-
+import Home from '../components/home';
 
 var routes = (
     <Router history={hashHistory}>
         <Route path="/" component={Main}>
-            <IndexRoute component={Main} />
+            <IndexRoute component={Home} />
             <Route path="/config" component={Config} />
             <Route path="/about" component={About} />
         </Route>
