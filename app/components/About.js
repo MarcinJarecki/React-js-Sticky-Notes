af0a06ff18da6e789c28c5782c5d677b91a8e3c2
MarcinJarecki <m.jarecki78@gmail.com>
2016-08-15 17:53:21 +0200

    Code climate configuration

--- app/components/About.js
+++ app/components/About.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default class About extends React.Component {
   render() {
-    return <div>About</div>;
+    return (<div>About</div>);
   }
 }
--- app/components/Config.js
+++ app/components/Config.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default class Config extends React.Component {
   render() {
-    return <div>Config</div>;
+    return (<div>Config</div>);
   }
 }
