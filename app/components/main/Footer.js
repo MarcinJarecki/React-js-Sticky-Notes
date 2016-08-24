62f1ff40090449fd987fc230a389de2f6500b4d9
MarcinJarecki <m.jarecki78@gmail.com>
2016-08-24 02:10:15 +0200

    Add footer

--- app/components/main/Footer.js
+++ app/components/main/Footer.js
@@ -5,7 +5,19 @@ var Footer = React.createClass({
   render: function() {
     return (
       <footer>
-        footer
+        <div className="container">
+          <div className="row-fluid">
+            <div className="span4">
+              <p>&copy; 2016 <a href="https://github.com/MarcinJarecki/">Marcin Jarecki</a></p>
+            </div>
+            <div className="span4">
+                <a href="https://github.com/MarcinJarecki/React-js-Sticky-Notes">Git repository</a>
+            </div>
+            <div className="span4 text-right">
+              <p>Powered By React.js and Bootstrap</p>
+            </div>
+          </div>
+        </div>
       </footer>
     );
   }
