070c87e33c093250d54d18e93cc0f28d77bbcb5b
MarcinJarecki <m.jarecki78@gmail.com>
2016-08-24 22:51:04 +0200

    add  components

--- app/components/LogIn.js
+++ app/components/LogIn.js
@@ -26,15 +26,15 @@ export default class LogIn extends React.Component {
                     <div className="form-group">
                       <a href="/account/provider/google" title="Google"
                         className="btn btn-block btn-social btn-lg btn-google">
-                        <i className="fa fa-google"></i> Sign in with Google
+                        <i className="fa fa-google"></i> Log in with Google
                       </a>
                       <a href="/account/provider/facebook" title="Facebook"
                         className="btn btn-block btn-social btn-lg btn-facebook">
-                        <i className="fa fa-facebook"></i> Sign in with Facebook
+                        <i className="fa fa-facebook"></i> Log in with Facebook
                       </a>
                       <a href="/account/provider/github" title="github"
                         className="btn btn-block btn-social btn-lg btn-github">
-                        <i className="fa fa-github"></i> Sign in with Github
+                        <i className="fa fa-github"></i> Log in with Github
                       </a>
                     </div>
 
