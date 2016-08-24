46bb73d9e1a8d1c20e5a9c9a3b5052d962b99e78
MarcinJarecki <m.jarecki78@gmail.com>
2016-08-24 13:40:20 +0200

    fix footer @copy

--- app/components/main/Footer.js
+++ app/components/main/Footer.js
@@ -9,7 +9,7 @@ var Footer = React.createClass({
         <div className="container">
           <div className="row-fluid">
             <div className="span4">
-              <p>&copy; 2016 &nbsp;
+              <p>&copy;2016 &nbsp;
                 <a style={aFontFooter} href="https://github.com/MarcinJarecki/">Marcin Jarecki</a>
               </p>
             </div>
